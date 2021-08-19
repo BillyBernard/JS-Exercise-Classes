497a7cf69f4a1f26cb532befebad1c8b53c5557d
BillyBernard <billybernard54@gmail.com>
2021-08-19 12:34:56 -0600

    finished task 1

--- index.js
+++ index.js
@@ -42,6 +42,26 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(someFood){
+    this.someFood = someFood;
+    if(this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+  }
+
+  poop(){
+    this.stomach = [];
+  }
+
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
   
 }
 
